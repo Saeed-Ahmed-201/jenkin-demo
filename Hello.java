e10e3266d9b7fa12e5a312996781fb4602c41621
saeedahmed000 <saeedsheikhdadu@gmail.com>
2021-01-31 22:17:39 +0500

    initial commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello{
+      public static void main(String args[]){
+
+         Scanner input = new Scanner(System.in);
+
+         String name = input.nextLine();
+         System.out.print(name);
+
+
+      }
+      
+}
+
